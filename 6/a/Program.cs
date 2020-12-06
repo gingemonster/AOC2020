9dfa5eec6b8d287126c1bf54127f017fdf46108a
gingemonster <brian@earthware.co.uk>
2020-12-06 06:18:06 +0000

    day 6

---
+++ 6/a/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllText("input.txt").Split(Environment.NewLine + Environment.NewLine);
+            var numberyes=0;
+            foreach(var pass in input){
+                var answers = pass.Replace(Environment.NewLine,"").ToArray();
+                numberyes += answers.GroupBy(x=>x).Count();
+            }
+            Console.WriteLine(numberyes);
+            Console.Read();
+        }
+    }
+}
---
+++ 6/b/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllText("input.txt").Split(Environment.NewLine + Environment.NewLine);
+            var numberyes=0;
+            foreach(var pass in input){
+                var numpeople = pass.Split(Environment.NewLine).Count();
+                var answers = pass.Replace(Environment.NewLine,"").ToArray();
+                numberyes += answers.GroupBy(x=>x).Select(g => new { Count = g.Count() }).Where(x=>x.Count==numpeople).Count();
+            }
+            Console.WriteLine(numberyes);
+            Console.Read();
+        }
+    }
+}
