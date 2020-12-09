61f078766f8ade39ccf9586c5e7ea9e94c06f42c
gingemonster <brian@earthware.co.uk>
2020-12-09 06:46:20 +0000

    day 9

---
+++ 9/a/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadLines("input.txt").Select(x=>Int64.Parse(x)).ToArray();
+            var preamblelength = 25;
+
+            for(var i=preamblelength;i<input.Length;i++){
+                // get previous pre-amble numbers
+                var preamble = input.Skip(i-preamblelength).Take(preamblelength).ToList();
+                if(!IsValid(preamble, input[i])){
+                    Console.WriteLine(input[i]);
+                    Console.Read();
+                }
+            }
+        }
+
+        private static bool IsValid(List<long> preamble, long totest){
+            // find two numbers in preamble that adds up to totest
+            for(var i=0;i<preamble.Count;i++){
+                if ((preamble.Any(x=>x == totest - preamble[i]) && preamble.IndexOf(totest - preamble[i]) != i) || preamble.Count(x=>x==totest - preamble[i]) > 1){
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ 9/b/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadLines("input.txt").Select(x=>Int64.Parse(x)).ToArray();
+            var invalidvalue = 57195069;
+
+            for(var i=0;i<input.Length;i++){
+                var sum = input[i];
+                var ii = i + 1;
+
+                // going to ignore checking for going over end of array as should have
+                // found solution by then
+                while(sum<invalidvalue){
+                    sum += input[ii];
+                    if(sum == invalidvalue){
+                        // get the range amd sort smallest to largest
+                        var rangesorted = input.Skip(i).Take(ii - i + 1).OrderBy(x=>x);
+                        // sum smallest and largest
+                        Console.WriteLine((rangesorted.First() + rangesorted.Last()));
+                        Console.Read();
+                    }
+                    if(sum > invalidvalue){
+                        break;
+                    }
+                    else{
+                        ii++;
+                    }
+                }
+            }
+        }
+    }
+}
