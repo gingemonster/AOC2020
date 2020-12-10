be735f822513fd7b52ce9e4e13094ad376f06990
gingemonster <brian@earthware.co.uk>
2020-12-10 09:17:46 +0000

    day 10 a fun one

---
+++ 10/a/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadLines("input.txt").Select(x=>Int64.Parse(x)).ToList();
+            input.Sort();
+            input.Insert(0,0); // outlet
+            var differences = input.Select((x,i)=> i + 1 < input.Count ? input[i+1]-x : 3);
+            var num1 = differences.Where(x=>x==1).Count();
+            var num3 = differences.Where(x=>x==3).Count();
+            Console.WriteLine(num1*num3);
+            Console.Read();
+        }
+    }
+}
---
+++ 10/b/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadLines("input.txt").Select(x=>Int64.Parse(x)).ToList();
+            input.Sort();
+            input.Insert(0,0); // outlet
+            var dict = new Dictionary<int,int>();
+            for(var i = 0; i < input.Count; i++){
+                var runlength = 0;
+                while(i + runlength < input.Count - 1 && input[i+runlength+1]-input[i+runlength] == 1){
+                    runlength++;
+                }
+                if(dict.ContainsKey(runlength)){
+                    dict[runlength] = dict[runlength] + 1;
+                }
+                else{
+                    dict[runlength] = 1;
+                }
+                i =  i + runlength;             
+            }
+            double perms = 1;
+            foreach(var run in dict)
+            {
+                if(run.Key < 2) continue;
+                for(var i = 0; i < run.Value; i++){
+                    var multiplier = Math.Pow(2,run.Key-1);
+                    if(run.Key==4) multiplier--;
+                    perms = perms * multiplier;
+                }
+            }
+            Console.WriteLine(perms);
+            Console.Read();
+        }
+    }
+}
