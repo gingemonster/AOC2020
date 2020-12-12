b205ec7d649b2390b138f104c2db820b1bb9179f
gingemonster <brian@earthware.co.uk>
2020-12-12 07:59:51 +0000

    day 12

---
+++ 12/a/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadLines("input.txt").Select(x=> new Tuple<string,long>(x.Substring(0,1),Int64.Parse(x.Substring(1)))).ToArray();
+            
+            long x = 0;
+            long y = 0;
+            long direction = 90;
+
+            foreach(var instruction in input){
+
+                var move = instruction.Item1 switch
+                {
+                    "N" => new Tuple<long,long>(0,instruction.Item2),
+                    "F" when direction==0 => new Tuple<long,long>(0,instruction.Item2),
+                    "S" => new Tuple<long,long>(0,-instruction.Item2),
+                    "F" when direction==180 => new Tuple<long,long>(0,-instruction.Item2),
+                    "E" => new Tuple<long,long>(instruction.Item2,0),
+                    "F" when direction==90 => new Tuple<long,long>(instruction.Item2,0),
+                    "W" => new Tuple<long,long>(-instruction.Item2,0),
+                    "F" when direction==270 => new Tuple<long,long>(-instruction.Item2,0),
+                    _ => new Tuple<long,long>(0,0)
+                };
+
+                if(instruction.Item1=="L"){
+                    direction -= instruction.Item2;
+                }
+                if(instruction.Item1=="R"){
+                    direction += instruction.Item2;
+                }
+                direction = direction % 360;
+
+                if (direction < 0)
+                {
+                    direction += 360;
+                }
+
+                // process move
+                x += move.Item1;
+                y += move.Item2;              
+            }
+
+            Console.WriteLine($"{Math.Abs(x) + Math.Abs(y)}");
+            Console.Read();
+        }
+    }
+}
---
+++ 12/b/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadLines("input.txt").Select(x=> new Tuple<string,long>(x.Substring(0,1),Int64.Parse(x.Substring(1)))).ToArray();
+            
+            long shipx = 0;
+            long shipy = 0;
+            long wpx = 10;
+            long wpy = 1;
+
+            foreach(var instruction in input){
+
+                var move = instruction.Item1 switch
+                {
+                    "N" => new Tuple<long,long>(0,instruction.Item2),
+                    "S" => new Tuple<long,long>(0,-instruction.Item2),
+                    "E" => new Tuple<long,long>(instruction.Item2,0),
+                    "W" => new Tuple<long,long>(-instruction.Item2,0),
+                    "F" => new Tuple<long,long>(instruction.Item2 * wpx, instruction.Item2 * wpy),
+                    _ => new Tuple<long,long>(0,0)
+                };
+
+                var wpcx = wpx;
+                var wpcy = wpy;
+                if(instruction.Item1=="R"){
+                    if(instruction.Item2==90) {
+                        // was was north becomes east, what was east becomes south
+                        wpx = wpcy;
+                        wpy = -wpcx;
+                    }
+                    else if(instruction.Item2==180){
+                        wpx = -wpcx;
+                        wpy = -wpcy;
+                    }
+                    else if(instruction.Item2==270){
+                        wpx = -wpcy;
+                        wpy = wpcx;
+                    }
+                }
+                else if(instruction.Item1=="L"){
+                    if(instruction.Item2==270) {
+                        wpx = wpcy;
+                        wpy = -wpcx;
+                    }
+                    else if(instruction.Item2==180){
+                        wpx = -wpcx;
+                        wpy = -wpcy;
+                    }
+                    else if(instruction.Item2==90){
+                        wpx = -wpcy;
+                        wpy = wpcx;
+                    }
+                }
+               
+                // process move
+                if(instruction.Item1=="F"){
+                    shipx += move.Item1;
+                    shipy += move.Item2;  
+                }
+                else{
+                    wpx += move.Item1;
+                    wpy += move.Item2;  
+                }
+            
+            }
+
+            Console.WriteLine($"{Math.Abs(shipx) + Math.Abs(shipy)}");
+            //Console.Read();
+        }
+    }
+}
