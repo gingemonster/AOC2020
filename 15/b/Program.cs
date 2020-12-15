70fb69565f08c49d074661078cfe2c9aaa3bbaff
gingemonster <brian@earthware.co.uk>
2020-12-15 14:39:08 +0000

    playing around with optimization of day 15

--- 15/b/Program.cs
+++ 15/b/Program.cs
@@ -4,13 +4,30 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using System.Collections.Generic;
 
-namespace _1
+namespace Day15
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var turns = new Dictionary<int,int>();
+            var watch = new System.Diagnostics.Stopwatch();
+            var timings = new List<long>();
+            for (var i = 0; i < 12; i++)
+            {
+                watch.Start();
+                Part2();
+                watch.Stop();
+                if (i > 1) timings.Add(watch.ElapsedMilliseconds);
+                watch.Reset();
+            }
+
+            Console.WriteLine(timings.Average());
+        }
+
+        private static void Part2()
+        {
+            var turns = new Dictionary<int,int>(29999999);
+
             //1,0,16,5,17,4
             turns.Add(1,0);
             turns.Add(0,1);
@@ -18,22 +35,18 @@ namespace _1
             turns.Add(5,3);
             turns.Add(17,4);
             
-            //turns.Add(2,6);
             var countstartingnumbers = turns.Count;
             var lastturn = 4;
-            for(var i=countstartingnumbers;i<30000000-1;i++){
+            for(var i=countstartingnumbers;i<29999999;i++){
                 var thisturn = 0;
-                if(!turns.ContainsKey(lastturn)){
-                    thisturn = 0;
-                }
-                else{
-                    thisturn = i-turns[lastturn];
+                var lastturnentry = turns.GetValueOrDefault(lastturn,-1);
+                if(lastturnentry != -1){
+                    thisturn = i-lastturnentry;
                 }
                 turns[lastturn] = i;
                 lastturn = thisturn;
             }
             Console.WriteLine(lastturn);
-            Console.Read();
         }
     }
 }
