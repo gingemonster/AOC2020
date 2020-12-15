83ceb38ea54028f562beea3842eda5035ffb744d
gingemonster <brian@earthware.co.uk>
2020-12-15 10:32:53 +0000

    day 15

---
+++ 15/a/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var turns = new List<int>() { 1,0,16,5,17,4 };
+            var countstartingnumbers = turns.Count;
+            for(var i=countstartingnumbers;i<2020;i++){
+                if(turns.Count <= countstartingnumbers || turns.IndexOf(turns[i-1]) == turns.Count-1){
+                    turns.Add(0);
+                }
+                else{
+                    var test1 = turns.Select((v, i) => new { Index = i, Value = v });
+                    var test2 = test1.Where(x=> x.Index!=turns.Count-1 && x.Value == turns[i-1]);
+                    var result = test2.Max(x=>x.Index);
+                    turns.Add(i - result - 1);
+                }
+            }
+
+            Console.WriteLine(turns.Last());
+            Console.Read();
+        }
+    }
+}
---
+++ 15/b/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var turns = new Dictionary<int,int>();
+            //1,0,16,5,17,4
+            turns.Add(1,0);
+            turns.Add(0,1);
+            turns.Add(16,2);
+            turns.Add(5,3);
+            turns.Add(17,4);
+            
+            //turns.Add(2,6);
+            var countstartingnumbers = turns.Count;
+            var lastturn = 4;
+            for(var i=countstartingnumbers;i<30000000-1;i++){
+                var thisturn = 0;
+                if(!turns.ContainsKey(lastturn)){
+                    thisturn = 0;
+                }
+                else{
+                    thisturn = i-turns[lastturn];
+                }
+                turns[lastturn] = i;
+                lastturn = thisturn;
+            }
+            Console.WriteLine(lastturn);
+            Console.Read();
+        }
+    }
+}
